e55e0259254f43b1cd0d1d3144667f65db03a7aa
Eric Kofman <ekofman@omicia.com>
2015-08-27 16:08:13 -0700

    Add VCF exportability

--- python/get_report_variants.py
+++ python/get_report_variants.py
@@ -24,7 +24,7 @@ OMICIA_API_URL = os.environ.get('OMICIA_API_URL', 'https://api.omicia.com')
 auth = HTTPBasicAuth(OMICIA_API_LOGIN, OMICIA_API_PASSWORD)
 
 
-def get_cr_variants(cr_id, statuses):
+def get_cr_variants(cr_id, statuses, _format):
     """Use the Omicia API to get report variants.
     """
     #Construct request
@@ -33,15 +33,21 @@ def get_cr_variants(cr_id, statuses):
 
     # Generate the url to be able to query for multiple statuses
     if statuses:
-        url = url + "?"
+        url += "?"
         for i, status in enumerate(statuses):
             if i > 0:
-                url = url + "&"
+                url += "&"
             url = url + "status={}".format(status)
+
+    # Add a paremeter for format. If not set, default is json
+    if _format == "VCF":
+        if not statuses:
+            url += "?"
+        url += '&format=VCF'
 
     sys.stdout.flush()
     result = requests.get(url, auth=auth)
-    return result.json()
+    return result
 
 
 def main():
@@ -50,22 +56,29 @@ def main():
     parser = argparse.ArgumentParser(description='Get variants for existing clinical reports.')
     parser.add_argument('cr_id', metavar='clinical_report_id', type=int)
     parser.add_argument('--status', metavar='status', type=str)
+    parser.add_argument('--format', metavar='_format', type=str, choices=['json', 'VCF'], default='json')
 
     args = parser.parse_args()
 
     cr_id = args.cr_id
     status = args.status
+    _format = args.format
 
     statuses = status.split(",") if status else None
 
-    json_reponse = get_cr_variants(cr_id, statuses)
-    try:
-        variants = json_reponse['objects']
-        for variant in variants:
-            print json.dumps(variant)
-            print '\n'
-    except KeyError:
-        print json_reponse
+    response = get_cr_variants(cr_id, statuses, _format)
+    if _format == 'VCF':
+        for block in response.iter_content(1024):
+            print block
+    else:
+        try:
+            response_json = response.json()
+            variants = response_json['objects']
+            for variant in variants:
+                print json.dumps(variant)
+                print '\n'
+        except KeyError:
+            print response.json()
 
 
 if __name__ == "__main__":
