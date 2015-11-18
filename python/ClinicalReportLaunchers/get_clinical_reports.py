fdd7a8e3b689065d50f9f6f88d995859fcf4e9ba
Eric Kofman <ekofman@omicia.com>
2015-11-18 15:29:50 -0800

    Get all clinical reports by genome id or accession id

--- python/ClinicalReportLaunchers/get_clinical_reports.py
+++ python/ClinicalReportLaunchers/get_clinical_reports.py
@@ -24,13 +24,20 @@ OMICIA_API_URL = os.environ.get('OMICIA_API_URL', 'https://api.omicia.com')
 auth = HTTPBasicAuth(OMICIA_API_LOGIN, OMICIA_API_PASSWORD)
 
 
-def get_clinical_reports(extended=False):
+def get_clinical_reports(accession_id, genome_id):
     """Use the Omicia API to get all clinical reports
     """
     # Construct request
     url = "{}/reports/"
-    if extended:
-        url += "?extended=True"
+    if any([accession_id, genome_id]):
+        url += "?"
+    if accession_id:
+        url += "accession_id={}&".format(accession_id)
+    if genome_id:
+        url += "genome_id={}&".format(genome_id)
+    # Remove any trailing '&'
+    if url.endswith('&'):
+        url = url[:-1]
     url = url.format(OMICIA_API_URL)
 
     sys.stdout.flush()
@@ -41,13 +48,18 @@ def get_clinical_reports(extended=False):
 def main():
     """Get all clinical reports.
     """
-    parser = argparse.ArgumentParser(description='Fetch all clinical report')
-    parser.add_argument('--e', metavar='extended', type=bool, default=False)
+    parser = argparse.ArgumentParser(description='Fetch all clinical reports, or search '
+                                                 'by accession id or genome id')
+    parser.add_argument('--a', metavar='acccession_id', type=str, default=False)
+    parser.add_argument('--g', metavar='genome_id', type=int, default=False)
+
     args = parser.parse_args()
 
-    extended = args.e
+    # Get query elements
+    accession_id = args.a
+    genome_id = args.g
 
-    json_response = get_clinical_reports(extended=extended)
+    json_response = get_clinical_reports(accession_id, genome_id)
     print json_response
 
 if __name__ == "__main__":
