ee7bb73067c43093e7b992d72375772328035a9a
Eric Kofman <ekofman@omicia.com>
2015-11-20 12:02:38 -0800

    Fix link genomes script

--- python/link_genome.py
+++ python/link_genome.py
@@ -5,6 +5,7 @@ import os
 import requests
 from requests.auth import HTTPBasicAuth
 import sys
+import argparse
 
 #Load environment variables for request authentication parameters
 if "OMICIA_API_PASSWORD" not in os.environ:
@@ -19,41 +20,49 @@ OMICIA_API_URL = os.environ.get('OMICIA_API_URL', 'https://api.omicia.com')
 auth = HTTPBasicAuth(OMICIA_API_LOGIN, OMICIA_API_PASSWORD)
 
 
-def upload_genome_to_project(project_id, label, sex, file_format, file_path, external_id=""):
+def upload_genome_to_project(project_id, sex, file_format, file_path, label="", external_id=""):
     """Use the Omicia API to add a genome, in vcf format, to a project.
     Returns the newly uploaded genome's id.
     """
-
-    assert(os.path.exists(file_path))
-
-    #Construct request
+    # Construct request
     url = "{}/projects/{}/genomes?genome_label={}&genome_sex={}&external_id={}\
            &assembly_version=hg19&format={}&file_path={}"
     url = url.format(OMICIA_API_URL, project_id, label, sex, external_id, file_format, file_path)
 
     sys.stdout.write("Linking...\n")
-    #Post request and return id of newly uploaded genome
+    # Post request and return id of newly uploaded genome
     result = requests.put(url, auth=auth)
     return result.json()
 
 
-def main(argv):
+def main():
     """main function. Upload a specified VCF file to a specified project.
     """
-    if len(argv) < 5:
-        sys.exit("Usage: python upload_genome.py <project_id> <label>"
-                 "<sex (male|female|unspecified)> <format> <file_path>")
-    project_id = argv[0]
-    label = argv[1]
-    sex = argv[2]
-    file_format = argv[3]
-    file_path = argv[4]
-    if len(argv) > 5:
-        external_id = argv[5]
-    else:
-        external_id = ""
-    json_response = upload_genome_to_project(project_id, label, sex,
-                                             file_format, file_path, external_id)
+    parser = argparse.ArgumentParser(
+        description='Create or edit a panel. Name and description must be '
+                    'specified for panel creation. Panel id must be '
+                    'specified for panel editing.')
+    parser.add_argument('project_id', metavar='project_id', type=int)
+    parser.add_argument('--label', metavar='label', type=str)
+    parser.add_argument('sex', metavar='sex', type=str, choices=['male', 'female', 'unspecified'])
+    parser.add_argument('file_format', metavar='file_format', type=str)
+    parser.add_argument('file_path', metavar='file_path', type=str)
+    parser.add_argument('--external_id', metavar='external_id', type=str)
+    args = parser.parse_args()
+
+    project_id = args.project_id
+    label = args.label
+    sex = args.sex
+    file_format = args.file_format
+    file_path = args.file_path
+    external_id = args.external_id
+
+    json_response = upload_genome_to_project(project_id,
+                                             sex,
+                                             file_format,
+                                             file_path,
+                                             label=label,
+                                             external_id=external_id)
 
     try:
         genome_object = json_response
@@ -68,4 +77,4 @@ def main(argv):
             sys.stdout.write('Something went wrong...')
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
