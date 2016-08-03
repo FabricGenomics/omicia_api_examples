0e36dec4512eab25f7f3cfc3991f1cdc7e0b3558
Ben Fagin <blouis@unquietcode.com>
2016-08-03 13:30:53 -0700

    Java example for downloading a clinical report

--- java/src/main/java/com/omicia/examples/OmiciaApiExample.java
+++ java/src/main/java/com/omicia/examples/OmiciaApiExample.java
@@ -3,10 +3,10 @@ package com.omicia.examples;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.FileEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.*;
 
 import java.io.File;
 import java.io.InputStream;
@@ -21,11 +21,11 @@ public class OmiciaApiExample {
 	static final String OMICIA_API_LOGIN = "";
 	static final String OMICIA_API_PASSWORD = "";
 
-	// Opal project ID
+	// an Opal project ID
 	static final String PROJECT_ID = "";
 
-	// label
-	static final String LABEL = "";
+	// an Opal clinical report ID
+	static final String REPORT_ID = "";
 
 	// sex must be one of 'male', 'female', or 'unspecified'
 	static final String SEX = "unspecified";
@@ -38,8 +38,21 @@ public class OmiciaApiExample {
 			throw new RuntimeException("usage: <file to upload>");
 		}
 
+		System.out.println("uploading a genome");
+		uploadGenome(PROJECT_ID, args[0]);
+		System.out.println();
+
+		System.out.println("downloading a report");
+		downloadReport(REPORT_ID);
+		System.out.println();
+
+		System.out.println("done! :)");
+	}
+
+	public static void uploadGenome(String projectID, String filePath) throws Exception {
+
 		// check input file for readability
-		File inputFile = new File(args[0]);
+		File inputFile = new File(filePath);
 
 		if (!inputFile.exists() || !inputFile.canRead()) {
 			throw new RuntimeException("unable to read input file '"+inputFile.getCanonicalPath()+"'");
@@ -51,8 +64,8 @@ public class OmiciaApiExample {
 		// build the URI fo the request
 		URI uri = new URIBuilder()
 			.setScheme("http").setHost(OMICIA_API_ENDPOINT)
-			.setPath("/projects/"+PROJECT_ID+"/genomes")
-			.setParameter("genome_label", LABEL)
+			.setPath("/projects/"+projectID+"/genomes")
+			.setParameter("genome_label", "Test R. Person")
 			.setParameter("genome_sex", SEX)
 			.setParameter("assembly_version", "hg19")
 			.setParameter("format", getFormatFromFile(inputFile.getName()))
@@ -68,13 +81,45 @@ public class OmiciaApiExample {
 		request.setEntity(new FileEntity(inputFile));
 
 		// execute the request
-		System.out.println("executing request");
+		System.out.println("executing request:");
 		System.out.println(request.getRequestLine());
 		HttpResponse response = httpClient.execute(request);
 		HttpEntity entity = response.getEntity();
 
 		// read the results
-		System.out.println("response received");
+		System.out.println("response received:");
+		String output = readStream(entity.getContent());
+		System.out.println(output);
+	}
+
+	public static void downloadReport(String reportID) throws Exception {
+
+		// create an http client
+		HttpClient httpClient = HttpClientBuilder.create().build();
+
+		// build the URI fo the request
+		URI uri = new URIBuilder()
+			.setScheme("http").setHost(OMICIA_API_ENDPOINT)
+			.setPath("/reports/"+reportID)
+			.setParameter("extended", "False")
+		.build();
+
+		// build the authorization header
+		String authorization = OMICIA_API_LOGIN+":"+OMICIA_API_PASSWORD;
+		authorization = Base64.getEncoder().encodeToString(authorization.getBytes());
+
+		// build the GET request
+		HttpGet request = new HttpGet(uri);
+		request.setHeader("Authorization", "Basic "+authorization);
+
+		// execute the request
+		System.out.println("executing request:");
+		System.out.println(request.getRequestLine());
+		HttpResponse response = httpClient.execute(request);
+		HttpEntity entity = response.getEntity();
+
+		// read the results
+		System.out.println("response received:");
 		String output = readStream(entity.getContent());
 		System.out.println(output);
 	}
